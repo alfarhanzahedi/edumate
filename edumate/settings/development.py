f8b06813fa82ed9a9636326e9b514c6558f0ad46
Alfarhan Zahedi <alfarhanzahedi@gmail.com>
2019-09-27 01:43:34 +0530

    Change development DBMS to SQLite

--- edumate/settings/development.py
+++ edumate/settings/development.py
@@ -9,12 +9,8 @@ INSTALLED_APPS += [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': config('DATABASE_HOST'),
-        'PORT': config('DATABASE_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 
