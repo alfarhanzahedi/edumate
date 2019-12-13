e38f6892eceb283eebbd0bd62215efb9f1bba381
Alfarhan Zahedi <alfarhanzahedi@gmail.com>
2019-12-14 04:50:43 +0530

    Move STATICFILES_DIRS from settings.development to settings.base

--- edumate/settings/base.py
+++ edumate/settings/base.py
@@ -105,6 +105,13 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATICFILES_DIRS = [
+    os.path.join('static'),
+]
+
 # Miscellaneous 
 
 AUTH_USER_MODEL = 'accounts.User'
