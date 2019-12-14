665e7d542e289cfcfb5f7260e428db4e6dd4bfa7
Alfarhan Zahedi <alfarhanzahedi@gmail.com>
2019-12-14 18:58:21 +0530

    Change STATIC_ROOT for development to project root

--- edumate/settings/development.py
+++ edumate/settings/development.py
@@ -18,7 +18,7 @@ INTERNAL_IPS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
