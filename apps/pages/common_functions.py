f9890f344373587fb52f67b765c895d458d3ad04
Alfarhan Zahedi <alfarhanzahedi@gmail.com>
2020-04-14 20:14:44 +0530

    Refactor code of 'pages' app

--- apps/pages/common_functions.py
+++ apps/pages/common_functions.py
@@ -10,13 +10,18 @@ def get_sidebar_context(request):
     context = {}
     context['left_sidebar'] = {}
     context['right_sidebar'] = {}
+
+    # The contents of the sidebars is different for teachers and students.
     if request.user.is_teacher:
         context['left_sidebar']['classroom_create_form'] = ClassroomCreationForm()
         context['left_sidebar']['classrooms'] = Classroom.objects.filter(teacher = request.user)
         context['left_sidebar']['exams'] = Exam.objects.filter(classroom__in = context['left_sidebar']['classrooms']).select_related('classroom')
+
     elif request.user.is_student:
         context['left_sidebar']['classrooms'] = request.user.student_of_classrooms.all()
         context['left_sidebar']['exams'] = request.user.exam_set.all().select_related('classroom').prefetch_related('submissions')
+
+    # Common contents.
     context['right_sidebar']['classroom_join_form'] = ClassroomJoinForm()
     context['right_sidebar']['exam_join_form'] = ExamJoinForm()
     return context
--- apps/pages/views.py
+++ apps/pages/views.py
@@ -12,21 +12,28 @@ from .common_functions import get_sidebar_context
 
 class LandingPage(View):
     def get(self, request):
-        if request.user.is_authenticated:
-            context = get_sidebar_context(request)
-            context['feed'] = {}
-            
-            classrooms = Classroom.objects.filter(Q(teacher = request.user) | Q(students__id = request.user.id))
-            post_list = Post.objects.select_related('user', 'classroom', 'classroom__teacher').filter(classroom__in = classrooms).order_by('-updated_at')
-            page = request.GET.get('page', 1)
-            paginator = Paginator(post_list, 2)
-            try:
-                posts = paginator.page(page)
-            except PageNotAnInteger:
-                posts = paginator.page(1)
-            except EmptyPage:
-                posts = paginator.page(paginator.num_pages)
+        # If the user is not authenticated, render the landing page;
+        # else, render the cumulative feed.
+        if not request.user.is_authenticated:
+            return render(request, 'pages/landing_page.html')
 
-            context['feed']['posts'] = posts
-            return render(request, 'pages/feed.html', context)
-        return render(request, 'pages/landing_page.html')
+        context = get_sidebar_context(request)
+
+        context['feed'] = {}
+
+        classrooms = Classroom.objects.filter(Q(teacher = request.user) | Q(students__id = request.user.id))
+        post_list = Post.objects.select_related('user', 'classroom', 'classroom__teacher').filter(classroom__in = classrooms).order_by('-updated_at')
+
+        posts = None
+        page = request.GET.get('page', 1)
+        paginator = Paginator(post_list, 2)
+
+        try:
+            posts = paginator.page(page)
+        except PageNotAnInteger:
+            posts = paginator.page(1)
+        except EmptyPage:
+            posts = paginator.page(paginator.num_pages)
+
+        context['feed']['posts'] = posts
+        return render(request, 'pages/feed.html', context)
